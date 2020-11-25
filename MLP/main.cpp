d3e042ae6f1e8a70a27f64fdbd8720375bf91b06
Joao Aurelio Francisco Junior <jafrancisco@RDL752507.raiadrogasil.com>
2020-11-25 14:14:20 -0300

    joao

--- MLP/main.cpp
+++ MLP/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <cstdlib>
+#include "mlp.cpp"
 
 using namespace std;
 
 int main(){
 
-    int a=0;
+    ANN net = ANN(4,2,5,3);
+    net.buildANN();
+    net.showNetworkConnections();
 
     return 0;
 
---
+++ MLP/mlp.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+class ANN
+{
+public:
+    int input;
+    int hidden;
+    int processors;
+    int output;
+    vector<list<int>> graph;
+
+    ANN(int input, int hidden, int processors, int output)
+    {
+        this->input = input;
+        this->hidden = hidden;
+        this->processors = processors;
+        this->output = output;
+        vector<list<int>> aux(input + output + (hidden * processors));
+        this->graph = aux;
+    }
+    void buildANN()
+    {
+        int cont = 0;
+        //adicionando primeiro as entradas
+        for (int i = 0; i < this->input; i++)
+        {
+
+            for (int j = this->input; j < this->input + this->processors; j++)
+            {
+                this->graph[i].push_back(j);
+            }
+        }
+        //adicionando ao grapo o restante dos nós
+        for (int i = this->input; i < this->graph.size()-this->output; i++)
+        {
+            if ((i+1) % this->processors == 0)
+                cont = 0;
+
+            for (int j = i + this->input - cont; j < i + this->input + this->processors - cont; j++)
+            {
+                this->graph[i].push_back(j);
+            }
+            cont++;
+        }
+    }
+
+    void showNetworkConnections()
+    {
+
+        list<int>::iterator it;
+
+        for (int i = 0; i < this->graph.size(); i++)
+        {
+            cout<<i<<" estah conectado a: ";
+            for (it = this->graph[i].begin(); it != this->graph[i].end(); it++)
+            {
+                cout<<*it<<" ";
+            }
+            cout<<endl;
+        }
+    }
+};
